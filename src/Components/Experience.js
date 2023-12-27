0056bf05a76982abdb8de21c617367fb896e0a7b
Megan Huckabay <huckabay90@gmail.com>
2023-12-26 23:42:21 -0800

    fixed all sizing styles to fit page better

--- src/Components/Experience.js
+++ src/Components/Experience.js
@@ -4,71 +4,77 @@ import "../Styles/Experience.css";
 const Experience = () => {
   return (
     <div className="experience-container experience-page">
+      <div className="experience-container1">
 
-      <div className='food-container'>
-        <h2>Fast Food Industry</h2>
-        <p>
-          <u>Little Caesars Arroyo Grande, CA. 2010-2011|
-          Jersey Mikes Subs, Sparks, NV. 2019</u>
-        </p>
-        <p>
-          During my time in the fast-food industry, I held various roles that contributed to the efficient
-          operation of the establishment. As an opening crew leader at Little Caesars, I was responsible for
-          setting up food prep stations, starting ovens, and checking inventory. This included placing orders
-          if inventory was low and making dough for pizza crust. Additionally, I worked in a fast-paced
-          establishment and worked well under pressure.
-        </p>
-        <p>
-          In my position as Cashier and deli slicer at Jersey Mikes Subs, I was responsible for slicing meats
-          and cheese for sandwiches and handling transactions and dishing out the best customer service. I
-          also had been trained for a manager after a month of working for Jersey Mikes.
-        </p>
-        <p>
-          Overall, my experience in fast food equipped me with strong multitasking abilities, excellent
-          customer service skills, and the capacity to thrive in a high-paced environment. I learned the
-          importance of teamwork and effective communication to ensure smooth operations and customer
-          satisfaction.
-        </p>
+        <div className='food-container'>
+          <h2>Fast Food Industry</h2>
+          <p>
+            <u>Little Caesars Arroyo Grande, CA. 2010-2011|
+              Jersey Mikes Subs, Sparks, NV. 2019</u>
+          </p>
+          <p>
+            During my time in the fast-food industry, I held various roles that contributed to the efficient
+            operation of the establishment. As an opening crew leader at Little Caesars, I was responsible for
+            setting up food prep stations, starting ovens, and checking inventory. This included placing orders
+            if inventory was low and making dough for pizza crust. Additionally, I worked in a fast-paced
+            establishment and worked well under pressure.
+          </p>
+          <p>
+            In my position as Cashier and deli slicer at Jersey Mikes Subs, I was responsible for slicing meats
+            and cheese for sandwiches and handling transactions and dishing out the best customer service. I
+            also had been trained for a manager after a month of working for Jersey Mikes.
+          </p>
+          <p>
+            Overall, my experience in fast food equipped me with strong multitasking abilities, excellent
+            customer service skills, and the capacity to thrive in a high-paced environment. I learned the
+            importance of teamwork and effective communication to ensure smooth operations and customer
+            satisfaction.
+          </p>
+        </div>
+
+
+
+        <div className='nurse-container'>
+          <h2>In-Home Care Nurse</h2>
+          <p><u>Private Clients/Family in Pismo Beach, CA. | Porterville, CA. | Tipton, CA.</u></p>
+          <p>
+            As an In-Home Care Nurse, my focus was on providing personalized care and support to individuals within the familiar setting of their homes. I handled responsibilities such as administering medications, closely monitoring vital signs, and assisting with day-to-day activities.
+
+            My commitment to detail, empathy, and effective communication was evident in every interaction. I aimed not only to ensure the physical well-being of my clients but also to create a supportive and comfortable environment. Beyond the medical aspects, I took pride in fostering independence by helping with daily activities, recognizing the significance of maintaining dignity in care.
+
+            I approached my role with a dedicated and empathetic mindset, tailoring care plans to meet the unique needs of each individual. Through open communication with both clients and their families, I ensured everyone was informed and involved in the care process, fostering trust and collaboration. In essence, my role as an In-Home Care Nurse was centered on holistic care that prioritized the overall health and comfort of those I served.
+          </p>
+        </div>
       </div>
 
-      <div className='nurse-container'>
-        <h2>In-Home Care Nurse</h2>
-        <p><u>Private Clients/Family in Pismo Beach, CA. | Porterville, CA. | Tipton, CA.</u></p>
-        <p>
-        As an In-Home Care Nurse, my focus was on providing personalized care and support to individuals within the familiar setting of their homes. I handled responsibilities such as administering medications, closely monitoring vital signs, and assisting with day-to-day activities.
+      <div className="experience-container2">
+        <div className='retail-container'>
+          <h2>Retail Position</h2>
+          <p>
+            <u>Levi's at the Pismo Beach Outlet Mall</u>
+          </p>
+          <p>
+            In my various roles within the retail industry, I acquired significant expertise in customer service and operational management. A key aspect of my responsibilities involved skillfully addressing customer inquiries, offering tailored product recommendations, and ensuring a consistently positive shopping experience.
 
-        My commitment to detail, empathy, and effective communication was evident in every interaction. I aimed not only to ensure the physical well-being of my clients but also to create a supportive and comfortable environment. Beyond the medical aspects, I took pride in fostering independence by helping with daily activities, recognizing the significance of maintaining dignity in care.
+            Beyond customer interactions, I took on the crucial tasks of maintaining store cleanliness, strategically restocking shelves, and efficiently managing inventory. This multifaceted experience in retail not only enhanced my organizational skills but also sharpened my attention to detail. Thriving in the fast-paced retail environment, I developed the ability to work efficiently and maintain a high level of productivity.
 
-        I approached my role with a dedicated and empathetic mindset, tailoring care plans to meet the unique needs of each individual. Through open communication with both clients and their families, I ensured everyone was informed and involved in the care process, fostering trust and collaboration. In essence, my role as an In-Home Care Nurse was centered on holistic care that prioritized the overall health and comfort of those I served.
-        </p>
-      </div>
+            My time in the retail sector was marked by a commitment to excellence, whether it was creating a welcoming atmosphere for customers or optimizing operational processes. This experience equipped me with a versatile skill set that goes beyond the register, encompassing a keen understanding of customer needs, effective inventory management, and the ability to excel in a dynamic and demanding workplace.
+          </p>
+        </div>
 
-      <div className='retail-container'>
-        <h2>Retail Position</h2>
-        <p>
-          <u>Levi's at the Pismo Beach Outlet Mall</u>
-        </p>
-        <p>
-        In my various roles within the retail industry, I acquired significant expertise in customer service and operational management. A key aspect of my responsibilities involved skillfully addressing customer inquiries, offering tailored product recommendations, and ensuring a consistently positive shopping experience.
+        <div className='youth-container'>
+          <h2>Youth Care Worker</h2>
+          <p>
+            <u>Youth Care Worker at Community, Counseling, and Corrections Service Inc.</u>
+          </p>
+          <p>
+            In my capacity as a Youth Care Worker, my primary focus was on delivering compassionate care and support to individuals with specific needs. I played a pivotal role in assisting with their daily activities, offering emotional support, and creating a safe and nurturing environment that fostered their overall well-being.
 
-        Beyond customer interactions, I took on the crucial tasks of maintaining store cleanliness, strategically restocking shelves, and efficiently managing inventory. This multifaceted experience in retail not only enhanced my organizational skills but also sharpened my attention to detail. Thriving in the fast-paced retail environment, I developed the ability to work efficiently and maintain a high level of productivity.
+            A significant aspect of my role involved collaborative efforts within a team of dedicated professionals. This collaborative environment allowed me to develop strong teamwork, effective communication, and adept problem-solving skills. Working in tandem with my colleagues, I contributed to the holistic care of the residents, ensuring that their unique needs were met with sensitivity and professionalism.
 
-        My time in the retail sector was marked by a commitment to excellence, whether it was creating a welcoming atmosphere for customers or optimizing operational processes. This experience equipped me with a versatile skill set that goes beyond the register, encompassing a keen understanding of customer needs, effective inventory management, and the ability to excel in a dynamic and demanding workplace.
-        </p>
-      </div>
-
-      <div className='youth-container'>
-        <h2>Youth Care Worker</h2>
-        <p>
-          <u>Youth Care Worker at Community, Counseling, and Corrections Service Inc.</u>
-        </p>
-        <p>
-        In my capacity as a Youth Care Worker, my primary focus was on delivering compassionate care and support to individuals with specific needs. I played a pivotal role in assisting with their daily activities, offering emotional support, and creating a safe and nurturing environment that fostered their overall well-being.
-
-        A significant aspect of my role involved collaborative efforts within a team of dedicated professionals. This collaborative environment allowed me to develop strong teamwork, effective communication, and adept problem-solving skills. Working in tandem with my colleagues, I contributed to the holistic care of the residents, ensuring that their unique needs were met with sensitivity and professionalism.
-
-        The essence of my work as a Youth Care Worker extended beyond mere assistance; it encompassed a commitment to creating a supportive community where individuals felt valued, understood, and empowered. Through a combination of hands-on care, emotional encouragement, and collaborative teamwork, I strived to enhance the quality of life for those under my care.
-        </p>
+            The essence of my work as a Youth Care Worker extended beyond mere assistance; it encompassed a commitment to creating a supportive community where individuals felt valued, understood, and empowered. Through a combination of hands-on care, emotional encouragement, and collaborative teamwork, I strived to enhance the quality of life for those under my care.
+          </p>
+        </div>
       </div>
     </div>
   );
